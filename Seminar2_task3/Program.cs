dd3cd957bf93517ccaff692f3beff9a78857ae75
KseniaAk <ksu909090@yandex.ru>
2022-10-24 21:50:20 +0300

    определяем число на палиндром

---
+++ Seminar2_task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+Console.Clear();
+Console.WriteLine("Введите число: "); 
+int num = int.Parse(Console.ReadLine() ?? "0");  // ноль не вводим
+int copy = num;
+int reverse = 0;
+while(copy > 0)
+{
+    int digit = copy % 10;
+    reverse = reverse * 10;
+    reverse = reverse + digit;
+    copy = copy / 10;
+}
+if (num == reverse)
+{
+    Console.WriteLine("Palindrome");
+}  
+else
+{
+    Console.WriteLine("Not palindrome");
+}   
