f6849547a83f7290607dab0ffa7945fc932a5e34
KseniaAk <ksu909090@yandex.ru>
2022-11-19 16:43:45 +0300

    сумма чисел от 1 до А

---
+++ Seminar4_task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число А и выдает сумму чисел от 1 до А
+
+Console.Clear();
+Console.Write("Введите число: ");
+Console.WriteLine($"Сумма цифр = {GetSumNums(num)}");
+//int num = int.Parse(Console.ReadLine());
+//int num2 = int.Parse(Console.ReadLine());
+//int sum1 = GetSumNums(num);
+//int sum2 = GetSumNums(num2);
+//Console.WriteLine(sum1);
+//Console.WriteLine(sum2);
+
+int GetSumNums(int number)
+{
+    int sum = 0;
+    while(number>0)
+    {
+        Console.Write($"{sum} ");
+        sum += number;
+        number--;
+    }
+    Console.WriteLine(number);
+    Console.WriteLine();
+    return sum;
+}
