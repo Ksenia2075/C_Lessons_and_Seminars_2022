3577be64b9938f1103ca6608694a831bba7cb6ed
KseniaAk <ksu909090@yandex.ru>
2022-11-24 11:23:23 +0300

    массив из 8 элементов версия 2

--- Seminar4_task4/Program.cs
+++ Seminar4_task4/Program.cs
@@ -1,11 +1,26 @@
 ﻿// Напишите программу, которая выводит массив из 8 элементов, заполненный
 // нулями и единицами в случайном порядке.
 Console.Clear();
-int[]array = new int[8];
 
-for(int i = 0; i < array.Length; i++)
+void FillArray(int[] arr)
 {
-    array[i] = new Random().Next(0,2);
-    Console.Write($"{array[i]} ");
+    int length = arr.Length;
+    for(int i = 0; i<length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
 }
-Console.WriteLine(array);
+
+void PrintArray(int[] arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i<length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+PrintArray(arr);
+Console.WriteLine();
