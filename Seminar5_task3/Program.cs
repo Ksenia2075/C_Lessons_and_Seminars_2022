d8c9a6039cc080d1e107446636e2af771fea3d32
KseniaAk <ksu909090@yandex.ru>
2022-11-24 16:33:01 +0300

    33. определяем присутствует ли заданное число в массиве

---
+++ Seminar5_task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// 33. Задайте массив. Напишите программу, которая определяет, присутствует ли 
+// заданное число в массиве
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+
+int[] array = GetArray(12,-9,9);
+Console.WriteLine();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+foreach (int el in array)
+{
+    if (el==num)
+    {
+        a += 1;
+    }
+}
+if (a>0) Console.WriteLine($"Да");
+else Console.WriteLine($"Нет");
