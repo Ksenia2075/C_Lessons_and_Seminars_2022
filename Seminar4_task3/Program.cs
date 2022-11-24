b40fe62e00ba55ca3b381521288cc1ea8605917a
KseniaAk <ksu909090@yandex.ru>
2022-11-24 10:26:28 +0300

    произведение чисел от 1 до N

---
+++ Seminar4_task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int i = GetCompos(num);
+int GetCompos(int number)
+{
+    int i = 1;
+    while(number>1)
+    {
+        i = i * number;
+        number--;
+    }
+    return i;
+}
+Console.WriteLine($"Произведение от 1 до {num} = {i}");
