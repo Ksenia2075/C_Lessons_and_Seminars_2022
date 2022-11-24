9e1924227e27d2f4e6d755d75d14160e19209a8e
KseniaAk <ksu909090@yandex.ru>
2022-11-24 16:59:33 +0300

    35. находим количество элементов массива в заданном отрезке

---
+++ Seminar5_task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// 35. Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, 
+// значения которых лежат в отрезке [10,99]. 
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i=0; i<size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+
+int[] array = GetArray(123,0,1000);
+Console.WriteLine();
+int count = 0;
+
+foreach (int el in array)
+{
+    if (el>9 && el<100) count++;
+}
+Console.WriteLine($"{count}");
+
