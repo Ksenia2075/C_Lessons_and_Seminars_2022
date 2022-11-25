4c0bc7c076d1f2313c5ee9036f1ce9ae18a73832
KseniaAk <ksu909090@yandex.ru>
2022-11-25 20:41:09 +0300

    двумерный массив строка

---
+++ Example012_Arraymatrix/Program.cs
@@ -0,0 +1,21 @@
+﻿// Двумерный массив
+// table[0, 0]  table[0, 1]  table[0, 2]
+// table[1, 0]  table[1, 1]  table[1, 2]
+Console.Clear();
+string[,] table = new string[2, 3];
+table[0, 0] = "A";
+table[0, 1] = "B";
+table[0, 2] = "C";
+table[1, 0] = "D";
+table[1, 1] = "E";
+table[1, 2] = "F";
+
+for(int i=0; i<2; i++)
+{
+    for(int j=0; j<3; j++)
+    {
+        Console.Write($"{table[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
