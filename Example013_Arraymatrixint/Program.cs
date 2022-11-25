8aca633d8fe6fc61462a33b47ba3105ce16e806c
KseniaAk <ksu909090@yandex.ru>
2022-11-25 20:48:11 +0300

    двумерный массив int

---
+++ Example013_Arraymatrixint/Program.cs
@@ -0,0 +1,14 @@
+﻿// Двумерный массив
+// matrix[0, 0]  matrix[0, 1]  matrix[0, 2]
+// matrix[1, 0]  matrix[1, 1]  matrix[1, 2]
+Console.Clear();
+int[,] matrix = new int[2, 3];
+
+for(int i=0; i<2; i++)
+{
+    for(int j=0; j<3; j++)
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+    Console.WriteLine();
+}
